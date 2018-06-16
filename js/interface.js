7e45f1bae5b89dec8a28cd87c134b762fd232950
Epat <kamikadze2185@gmail.com>
2018-06-16 20:14:19 +0200

    cards concept

--- js/interface.js
+++ js/interface.js
@@ -1,5 +1,51 @@
 let elements = {};
 let inst = {};
+let win = {
+  w: window.innerWidth,
+  h: window.innerHeight,
+};
+
+let resize = function () {
+  win.w = window.innerWidth;
+  win.h = window.innerHeight;
+  console.log(win);
+};
+
+let scrollCards = function (e) {
+  // console.log(e);
+  let h = document.querySelector('header');
+  let hc = document.querySelector('header').children[0];
+  let cards = document.querySelectorAll('.card');
+
+  for (var i = 0; i < cards.length - 1; i++) {
+    let el = cards[i].children[0];
+
+    let b = cards[i].getBoundingClientRect().bottom;
+    if (b <= win.h) {
+      let p = win.h - b;
+      if (p <= win.h) {
+        let sc = 1 - (p / win.h);
+        if (sc <= 0.8) {
+          sc = 0.8;
+        }
+
+        el.style.transform = `scale(${sc})`;
+        el.style.top = `0.${i}vh`;
+        el.style.position = 'fixed';
+        el.style.zIndex = `0.0000${i}`;
+        el.style.boxShadow = '0 0 5px 0 black';
+      }
+    } else {
+      el.style.transform = `scale(1)`;
+      el.style.top = '';
+      el.style.position = '';
+      el.style.zIndex = ``;
+      el.style.boxShadow = '';
+    }
+
+  }
+
+};
 
 //elements of DOM
 elements.mbtn = document.getElementById('mbtn');
@@ -21,3 +67,8 @@ inst.clMenu = function () {
 
 elements.mbtn.addEventListener('click', inst.opMenu);
 elements.bbtn.addEventListener('click', inst.clMenu);
+
+window.addEventListener('resize', resize);
+document.addEventListener('scroll', scrollCards);
+
+scrollCards();
