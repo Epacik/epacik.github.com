5ee8cfea4daf174e581c67ecda160b96fdd3fef3
Epat <epaciatko@gmail.com>
2019-01-06 16:03:14 +0100

    webpage update

---
+++ scripts/main.js
@@ -0,0 +1,79 @@
+var isIE = /*@cc_on!@*/false || !!document.documentMode;
+
+if (isIE) {
+    alert('This webpage isn\'t supporting Internet Explorer.\nIf you want to use that page, you have to use newer web browser like:\nGoogle Chrome, Mozilla Firefox, Opera, Apple Safari, Microsoft Edge etc.');
+}
+
+console.log('loaded');
+
+/**
+ * @function toggleLoading
+ * @desc Toggles loading screen
+ */
+function toggleLoading() {
+    loading = document.querySelector('.page-loading');
+    if (loading.style.opacity == '0') {
+        loading.style.opacity = '';
+        loading.style.pointerEvents = '';
+        loading.style.transform = "scale(1)";
+        document.body.style.overflow = '';
+        document.querySelector('.page-loading #spinner div').style.animationName = 'spin';
+    } else {
+        loading.style.opacity = '0';
+        loading.style.pointerEvents = 'none';
+        document.body.style.overflow = 'auto';
+        loading.style.transform = "scale(2)";
+        document.querySelector('.page-loading #spinner div').style.animationName = '';
+    }
+
+}
+
+function applyLayout() {
+    let layout = document.querySelector(".layoutTemplate").innerHTML;
+    let pages = document.querySelectorAll(".subpage");
+
+    for (let i = 0; i < pages.length; i++) {
+        let content = pages[i].innerHTML;
+        pages[i].innerHTML = layout;
+        pages[i].querySelector(".spContent").innerHTML = content;
+    }
+}
+
+function showPage() {
+    let hash = location.hash;
+    hash = hash.toLocaleLowerCase();
+    hash = hash.replace("#", "");
+    hash = hash.split("/")
+    let pages = document.querySelectorAll(".subpage");
+    let moved = false;
+    for (let i = 0; i < pages.length; i++) {
+        if (pages[i].id == hash[0]) {
+            moved = true;
+            pages[i].classList.add("show");
+        } else {
+            pages[i].classList.remove("show");
+        }
+    }
+
+    let navlinks = document.querySelectorAll(".nav-link");
+
+    for (let i = 0; i < navlinks.length; i++){
+        if (navlinks[i].href.split("#")[1].split("/")[0] == hash[0]) {
+            navlinks[i].parentElement.classList.add("active");
+        } else {
+            navlinks[i].parentElement.classList.remove("active");
+        }
+    }
+
+    if (!moved) {
+        document.getElementById("home").classList.add("show");
+    }
+}
+
+applyLayout();
+
+showPage();
+
+window.addEventListener("hashchange", showPage);
+
+toggleLoading();
