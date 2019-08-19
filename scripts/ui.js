3d362399391f454cff538962e45b15067979f09e
epacik <epaciatko@gmail.com>
2019-08-19 22:03:30 +0200

    mainly layout update

--- scripts/ui.js
+++ scripts/ui.js
@@ -5,8 +5,20 @@
     panel.scrollIntoView({ behavior: "smooth", block: "start" });
 }
 
+let collapseNavMenu = true;
+
 function ToggleNavMenu(button) {
+    collapseNavMenu = !collapseNavMenu;
+    let nav = document.getElementById(button.dataset.target);
+    nav.classList.replace("collapse", "collapsing");
     
+
+    setTimeout(() => nav.style.height = collapseNavMenu ? "0px" : "248px", 100)
+    
+    setTimeout(() => {
+        nav.classList.toggle("show");
+        nav.classList.replace("collapsing", "collapse");
+    }, 500);
 }
 
 setInterval(() => {
@@ -18,3 +30,4 @@ setInterval(() => {
     let spinner = document.getElementById("contactSpinner");
     if (spinner != null && cards.length != 0) spinner.parentNode.removeChild(spinner);
 }, 1000);
+
